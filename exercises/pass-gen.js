acbfa1b963cbb28611382c2f91d1b666c56e8528
pfcardona <pfcardona@gmail.com>
2022-11-18 12:04:31 -0500

    sd

---
+++ exercises/pass-gen.js
@@ -0,0 +1,15 @@
+/**
+ * todo Generador de Contraseñas | Ejercicio práctico JavaScript
+*/ 
+
+const alph = "ABCDEFGHIJKLMNOPQRSTUVWYZabcdefghijqlmnopqrstuvwxyz0123456789!#$%&/()=?¡"
+
+function passGenerate(length = 10) {
+    let result = ""
+    for(let i = 0; i<=length; i++){
+        result += alph.charAt(Math.floor(Math.random() * alph.length))
+    }
+    return result
+}
+const pass = passGenerate(30)
+console.log(pass);
