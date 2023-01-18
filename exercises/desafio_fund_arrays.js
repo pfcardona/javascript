3555467ed04aeb7da5eb3d697caf3de65392f8ab
pfcardona <pfcardona@gmail.com>
2023-01-18 10:25:08 -0500

    change

---
+++ exercises/desafio_fund_arrays.js
@@ -0,0 +1,72 @@
+/**
+*todo: Tienes un array de objetos que representan datos de productos con los siguientes atributos:
+
+    name
+    price
+    stock
+Tu reto es agregar y calcular una nueva propiedad llamada "taxes", los impuestos deben ser del 19%
+con base al precio base y debes tener en cuenta que como resultado se debe dejar un valor entero, sin decimales.
+
+*? Por ejemplo si aplicamos el 19% de impuestos para un producto con precio de $1000 el resultado de los "taxes" será $190,
+*? o para un producto con precio de $656 el resultado de los "taxes" será $124.
+*? Para solucionarlo vas a encontrar una función llamada addNewAttr que recibe un parámetro de entrada:
+
+array: Un array de objetos.
+*! Dentro del cuerpo de la función addNewAttr debes escribir tu solución.
+
+*/
+/* Input:
+
+
+[
+    {
+    name: "Product 1",
+    price: 1000,
+    stock: 10
+    },
+    {
+    name: "Product 2",
+    price: 2000,
+    stock: 20
+    }
+]
+
+Output:
+[
+    {
+    name: "Product 1",
+    price: 1000,
+    stock: 10,
+    taxes: 190
+    },
+    {
+    name: "Product 2",
+    price: 2000,
+    stock: 20,
+    taxes: 380
+    }
+]
+*/
+array = [
+    {
+    name: "Product 1",
+    price: 1000,
+    stock: 10
+    },
+    {
+    name: "Product 2",
+    price: 2000,
+    stock: 20
+    }
+]
+
+function addNewAttr(array) {
+
+    return array.map((item) => {
+        const copyItem = { ...item };
+        copyItem.taxes = Math.trunc(copyItem.price * 0.19)
+        console.log(copyItem);
+    });
+    }
+
+addNewAttr(array)
