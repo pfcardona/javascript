d2e44e63b173d2e88eb6a1d168020a695356def8
pfcardona <pfcardona@gmail.com>
2023-02-21 07:50:24 -0500

    change pomodor

--- js/dom/reloj.js
+++ js/dom/reloj.js
@@ -1,1 +1,20 @@
 /* **********     Curso JavaScript: 82. DOM: Ejercicios Prácticos | Reloj Digital - #jonmircha     ********** */
+const d = document;
+
+function digitalClock(clock,btnPlay,btnStop) {
+    d.addEventListener("click", e =>{
+        if(e.target.matches(btnPlay)) {
+            setInterval(() => {
+                let clockHour = new Date().toLocaleTimeString()
+                d.querySelector(clock).innerHTML = `<h3>${clockHour}</h3>`
+            }
+            ,1000)
+
+            e.target.disabled = true
+        }
+        if(e.target.matches(btnStop)){
+
+        }
+
+    })
+}
